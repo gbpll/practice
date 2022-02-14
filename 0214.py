0f055f903434dc0ce632e9b4278ef97472a59f26
gbpll <pollack29@naver.com>
2022-02-14 19:16:09 +0900

    Create 0214.py

---
+++ 0214.py
@@ -0,0 +1,71 @@
+# a, b, c = map(int, input().split())
+# print(a, b, c)
+
+# #f
+# a = 7
+# print(f"정답은 {a}입니다.")
+
+# a = 15
+# if a<10:
+#     print("a<10")
+
+# elif a>=20:
+#     print("a>=20")
+# else :
+#     print("a = 15")
+
+# score = 85
+# if score >= 90:
+#     print("학점: A")
+# elif score >= 80:
+#     print("학점: B")
+# elif score >=70:
+#     print("학점: C")
+# else:
+#     print("학점: F")
+
+# score = 80
+# result = "success" if score >=75 else "fail"
+# print(result)
+
+#while
+# from re import I
+
+
+# i = 1
+# result = 0
+# # while i <= 9:
+#     result += i
+#     i += 1
+# print(result)
+
+# while i <= 9:
+#     if i % 2 ==1:
+#         result += i
+#     i +=1
+# print(result)
+
+#for
+# array = [9, 8, 7, 6, 5]
+# for x in array:
+#     # print(x)
+# print(sum[])
+
+# result = 0
+
+# # for i in range(1, 31):
+# #     result += i
+
+# # print(result)
+
+# #1~9 홀수의 합
+# for i in range(1,10):
+#     if i % 2 ==0:
+#         continue #건너 뜀
+#     result +=i
+# print(result)
+
+scores=[90, 85, 77, 65, 97]
+for i in range(5):
+    if scores[i]>=80:
+        print(i+1,"번 학생은 합격입니다.")
